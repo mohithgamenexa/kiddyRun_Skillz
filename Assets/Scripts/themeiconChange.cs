127991c96e94e12245103e7a1ea18767b06b0393
dharmateja443 <29258574+dharmateja443@users.noreply.github.com>
2024-06-21 14:47:04 +0530

    portal fx changed

--- Assets/Scripts/themeiconChange.cs
+++ Assets/Scripts/themeiconChange.cs
@@ -6,9 +6,12 @@ public class themeiconChange : MonoBehaviour
 {
     // Start is called before the first frame update
     public Texture2D[] themicons;
+    public Sprite[] portalImgs;
+    public GameObject imgHolder;
     void Start()
     {
         GetComponent<Renderer>().material.mainTexture = themicons[TrackManager._instance.NextZone];
+        imgHolder.GetComponent<SpriteRenderer>().sprite = portalImgs[TrackManager._instance.NextZone];
         if(Random.Range(0,10) < 5)
         {
             Vector3 pos = transform.parent.localPosition;
